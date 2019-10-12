143406d9f7e323d4afe19f026d99877d239a42c3
unknown <akinayodeji4all@gmail.com>
2019-10-12 23:42:25 +0100

    Intial commit

---
+++ src/scenes/index.js
@@ -0,0 +1,15 @@
+import React , { Component } from "react";
+import Home from "./Home";
+
+class App extends Component{
+    constructor(){
+        super()
+    }
+    render(){
+        return(
+               <Home/>
+        )
+    }
+}
+
+export default  App
